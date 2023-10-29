0c4a4603df9fc1502bde80ed46dcc12a50c1e0c4
Colin Beltz <colinbeltz@gmail.com>
2023-10-29 13:58:55 -0500

    Routes intro for NextJS

---
+++ app/about/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutID({ params }: any) {
+    return (
+      <div>
+          <h1>This is the About page for ID: {params.id} </h1>
+      </div>
+    );
+  }
+  
---
+++ app/about/us/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutUs() {
+    return (
+      <div>
+          <h1>This is the About Us page</h1>
+      </div>
+    );
+  }
+  
